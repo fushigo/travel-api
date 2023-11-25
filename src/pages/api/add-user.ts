2334767ade98fe369eacfad6e7322005bace253a
Fushigo <rizky23ska@gmail.com>
2023-11-25 10:03:03 +0700

    get-user

--- src/pages/api/add-user.ts
+++ src/pages/api/add-user.ts
@@ -1,10 +1,19 @@
 import { cors, middleware } from "@/helpers/middleware";
 import prisma from "../../../prisma/client";
 
+interface UserRequest {
+  body: {
+    nama: string;
+    username: string;
+    email: string;
+    password: string;
+  };
+}
+
 export default async function handler(req: any, res: any) {
   await middleware(req, res, cors);
   if (req.method === "POST") {
-    const { nama, username, email, password } = req.body;
+    const { nama, username, email, password } = (req as UserRequest).body;
     try {
       const user = await prisma.users.create({
         data: {
