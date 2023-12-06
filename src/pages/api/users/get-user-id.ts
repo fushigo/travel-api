85547c299967cfc2819bb14b030eb7d4a8606c91
Fushigo <rizky23ska@gmail.com>
2023-12-07 02:35:47 +0700

    add getUserId

--- src/pages/api/users/get-user-id.ts
+++ src/pages/api/users/get-user-id.ts
@@ -6,18 +6,16 @@ export default async function handler(req: Request, res: Response) {
   await middleware(req, res, cors);
 
   if (req.method === "GET") {
-    const { skip } = req.query;
-    const skipValue = skip ? Number(skip) : 0;
+    const { id } = req.query;
     try {
-      const list = await prisma.users.findMany({
-        skip: skipValue,
+      const list = await prisma.users.findUnique({
+        where: {
+          id: Number(id),
+        },
       });
-
-      const countData = await prisma.users.count();
 
       res.status(200).json({
         message: "Success",
-        totalData: countData,
         data: list,
       });
     } catch (e) {
