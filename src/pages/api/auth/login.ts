673680bda159e2f4cf23f0c55713e67fc70726f3
Fushigo <rizky23ska@gmail.com>
2023-12-07 16:00:40 +0700

    update login

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -28,6 +28,7 @@ export default async function handler(req: Request, res: Response) {
           res.status(200).json({
             message: "Success",
             data: {
+              id: response.id,
               nama: response.nama,
               username: response.username,
               email: response.email,
