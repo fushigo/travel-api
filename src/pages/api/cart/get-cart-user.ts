5e937ad65d4bf59122b306529cb0aab1ceeefcca
Fushigo <rizky23ska@gmail.com>
2023-12-10 07:41:30 +0700

    add price

--- src/pages/api/cart/get-cart-user.ts
+++ src/pages/api/cart/get-cart-user.ts
@@ -23,14 +23,22 @@ export default async function handler(req: Request, res: Response) {
         },
       });
 
+      const countCartData = userCart?.cart.length;
+
       const totalHarga = userCart?.cart.map((items, key) => {
         return items.product.harga * items.quantity;
       });
 
+      const payment = totalHarga?.reduce((acc, total) => {
+        return acc + total;
+      }, 0);
+
       res.status(201).json({
         message: "Cart found successfully",
+        totalCartData: countCartData,
         data: userCart,
         totalHarga: totalHarga,
+        payment: payment,
       });
     } catch (error) {
       res.status(500).json({ error });
